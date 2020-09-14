c69f218626c3fc75f93f6254f466f448c0dcbdcc
asrivastava <asrivastav@gainsight.com>
2020-09-15 00:22:22 +0530

    Add option to implement placeholder

--- src/components/placeholder/FileUploaderPlaceholder.tsx
+++ src/components/placeholder/FileUploaderPlaceholder.tsx
@@ -5,11 +5,9 @@ import "./FileUploaderPlaceholder.css"
 
 export const FileUploaderPlaceholder = () => {
   return (
-      <div className="UploaderPlaceholderWrapper">
-        <div className="UploaderPlaceholder">
-          <img className="UploaderPlaceholderImage" alt={"Drop files to upload"} src={Placeholder}/>
-          <div className="UploaderPlaceholderText">Drop files to upload</div>
-        </div>
+      <div className="UploaderPlaceholder">
+        <img className="UploaderPlaceholderImage" alt={"Drop files to upload"} src={Placeholder}/>
+        <div className="UploaderPlaceholderText">Drop files to upload</div>
       </div>
   )
 };
